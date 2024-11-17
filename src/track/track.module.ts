b50d3705108e4cfad70a2fc5a7d5b7ec391ae753
anturchin <tur4intwo@gmail.com>
2024-11-17 21:34:29 +0300

    feat: connect prisma to track

--- src/track/track.module.ts
+++ src/track/track.module.ts
@@ -3,9 +3,10 @@ import { TrackController } from './track.controller';
 import { TrackService } from './track.service';
 import { TrackRepository } from './track.repository';
 import { FavoritesModule } from '../favorites/favorites.module';
+import { DatabaseModule } from '../database/database.module';
 
 @Module({
-  imports: [forwardRef(() => FavoritesModule)],
+  imports: [forwardRef(() => FavoritesModule), DatabaseModule],
   controllers: [TrackController],
   providers: [TrackService, TrackRepository],
   exports: [TrackService, TrackRepository],
