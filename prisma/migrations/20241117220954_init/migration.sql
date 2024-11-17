0f6e160bf7ecc07bfac86e4c4b71d3edaa2d7e26
anturchin <tur4intwo@gmail.com>
2024-11-18 01:11:50 +0300

    feat: update migrations

--- prisma/migrations/20241117220954_init/migration.sql
+++ prisma/migrations/20241117220954_init/migration.sql
@@ -1,4 +1,16 @@
 -- CreateTable
+CREATE TABLE "User" (
+    "id" TEXT NOT NULL,
+    "login" TEXT NOT NULL,
+    "password" TEXT NOT NULL,
+    "version" INTEGER NOT NULL DEFAULT 1,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" TIMESTAMP(3) NOT NULL,
+
+    CONSTRAINT "User_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
 CREATE TABLE "Artist" (
     "id" TEXT NOT NULL,
     "name" TEXT NOT NULL,
@@ -26,16 +38,6 @@ CREATE TABLE "Album" (
     "artistId" TEXT,
 
     CONSTRAINT "Album_pkey" PRIMARY KEY ("id")
-);
-
--- CreateTable
-CREATE TABLE "Favorites" (
-    "id" TEXT NOT NULL,
-    "artists" TEXT[],
-    "albums" TEXT[],
-    "tracks" TEXT[],
-
-    CONSTRAINT "Favorites_pkey" PRIMARY KEY ("id")
 );
 
 -- AddForeignKey
